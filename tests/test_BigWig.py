7226a613fbb5da2d287a9a12324c7fea9607bdb9
jkanche <jayaram.kancherla@gmail.com>
2020-12-30 23:18:25 -0500

    add bigwig tests

---
+++ tests/test_BigWig.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import os
+
+from parser.BigWig import BigWig
+
+__author__ = "jkanche"
+__copyright__ = "jkanche"
+__license__ = "mit"
+
+"""
+    The file test.bigBed and test assertions 
+    come from the pyBigWig library
+"""
+
+bb = BigWig("tests/test.bw")
+
+def test_header():
+    assert(bb.header == {'magic': 2291137574, 'version': 4, 'zoomLevels': 1, 'chromTreeOffset': 344, 'fullDataOffset': 400, 'fullIndexOffset': 487, 'fieldCount': 0, 'definedFieldCount': 0, 'autoSqlOffset': 0, 'totalSummaryOffset': 304, 'uncompressBufSize': 32768})
+
+def test_columns():
+    assert(len(bb.columns) == 4)
+    assert(bb.columns == ["chr", "start", "end", "score"])
+
+def test_range():
+    res, err = bb.getRange(chr="1", start=0, end=3)
+    assert(err == None)
+    assert(len(res) == 3)
