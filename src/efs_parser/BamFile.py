c9fc7d062494596bc6f72182b8ba97486c97a7c9
jkanche <jayaram.kancherla@gmail.com>
2021-01-21 20:09:42 -0500

    cleanup msgpack

--- src/efs_parser/BamFile.py
+++ src/efs_parser/BamFile.py
@@ -45,11 +45,11 @@ class BamFile(SamFile):
         """
         return toDataFrame(result, self.columns)
 
-    def to_msgpack(self, result):
-        """
-        return object as msgpack
-        """
-        return toMsgpack(result)
+    # def to_msgpack(self, result):
+    #     """
+    #     return object as msgpack
+    #     """
+    #     return toMsgpack(result)
 
     def get_col_names(self, result):
         """
