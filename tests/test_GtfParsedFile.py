65c8b228417411e62c3eac317def27fb9fc4806e
elgamlh <Canada2012:)>
2021-01-27 12:48:44 -0500

    new fork commit

--- tests/test_GtfParsedFile.py
+++ tests/test_GtfParsedFile.py
@@ -8,8 +8,9 @@ from efs_parser.GtfParsedFile import GtfParsedFile
 __author__ = "jkanche, elgaml"
 __copyright__ = "jkanche"
 __license__ = "mit"
-
-bb = GtfParsedFile("tests/genes.tsv.gz")
+# hany is reforking
+#bb = GtfParsedFile("tests/genes.tsv.gz")
+bb = GtfParsedFile("https://raw.github.com/epiviz/efs-genomes/master/hg38/genes.tsv.gz")
 
 def test_columns():
     assert (bb.columns==['chr', 'start', 'end', 'width', 'strand', 'geneid', 'exon_starts', 'exon_ends', 'gene'])
