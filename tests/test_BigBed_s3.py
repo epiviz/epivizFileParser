0cbd90cb1526b0222148d5f6c084042bd924e144
Hany <hany.elgaml@contractors.roche.com>
2021-07-08 15:59:05 -0400

    Write tests for BigBed,BigWig files on S3

---
+++ tests/test_BigBed_s3.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import os
+
+from efs_parser.BigBed import BigBed
+
+__author__ = "jkanche, elgaml"
+__copyright__ = "jkanche"
+__license__ = "mit"
+
+"""
+    The file test.bigBed and test assertions 
+    come from the pyBigWig library
+"""
+
+bb = BigBed("s3://encode-public/2008/11/24/0868284e-8c3c-488d-89e6-487cd89971c3/ENCFF000AAU.broadPeak.bigbed")
+
+def test_header():
+    assert(bb.header == {'magic': 2273964779, 'version': 4, 'zoomLevels': 9, 'chromTreeOffset': 1239, 'fullDataOffset': 1600, 'fullIndexOffset': 13614032, 'fieldCount': 9, 'definedFieldCount': 6, 'autoSqlOffset': 304, 'totalSummaryOffset': 1135, 'uncompressBufSize': 16384})
+
+def test_columns():
+    assert(len(bb.columns) == 9)
+    assert(bb.columns == ["chr", "start", "end", "name", "score", "strand", "signalValue", "pValue", "qValue"])
+
+def test_range():
+    res, err = bb.getRange(chr="chr1", start=10000000, end=10020000)
+    assert(err == None)
+    assert(len(res) == 4)
+
+def test_get_bytes():
+    res = bb.get_bytes(1, 100)
+    assert (len(res) == 100)
---
+++ tests/test_BigWig_s3.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import os
+
+from efs_parser.BigWig import BigWig
+
+__author__ = "jkanche, elgaml"
+__copyright__ = "jkanche"
+__license__ = "mit"
+
+"""
+    The file test.bigBed and test assertions 
+    come from the pyBigWig library
+"""
+
+bb = BigWig("s3://encode-public/2010/09/17/908fcd8c-9d81-4134-821e-0e9fae69be77/ENCFF000LMN.bigWig")
+
+def test_header():
+    assert(bb.header == {'magic': 2291137574, 'version': 4, 'zoomLevels': 10, 'chromTreeOffset': 344, 'fullDataOffset': 705, 'fullIndexOffset': 212734456, 'fieldCount': 0, 'definedFieldCount': 0, 'autoSqlOffset': 0, 'totalSummaryOffset': 304, 'totalSummaryOffset': 304,'uncompressBufSize':32768})
+
+def test_columns():
+    assert(len(bb.columns) == 4)
+    assert(bb.columns == ["chr", "start", "end", "score"])
+
+def test_range():
+
+    res, err = bb.getRange(chr="chr4", start=0, end=3)
+    assert (err == None)
+    assert (len(res) == 0)
+
+def test_get_bytes():
+    res = bb.get_bytes(1, 100)
+    assert (len(res) == 100)
+
+def test_bin_rows():
+    #bb = BigWig("./ENCFF685PIH.bigWig")
+    start = 5000000
+    end = 10020000
+    res, err = bb.getRange(chr="chr1", start=start, end=end)
+    v = bb.bin_rows(data=res, chr="chr1", start=start, end=end, columns=['score'], bins=10)
+    assert (len(v[0]) == 10)
+
+def test_simplified_bin_rows():
+    #bb = BigWig("./39031.bigWig")
+    start = 5000000
+    end = 10020000
+    res, err = bb.getRange(chr="chr1", start=start, end=end)
+    v = bb.simplified_bin_rows(data=res, chr="chr1", start=start, end=end, columns=['score'], bins=10)
+    assert (len(v[0]) == 10)
