29f7b81b5d75ce8d219a51ed1c66316b4938230c
Victor Alexandru <victor.alexandru@contractors.roche.com>
2021-09-02 14:05:59 -0400

    fix: uncommented files

--- tests/test_BigBed_remote.py
+++ tests/test_BigBed_remote.py
@@ -1,81 +1,81 @@
-# # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-# import pytest
-# import os
+import pytest
+import os
 
-# from epivizFileParser.BigBed import BigBed
+from epivizFileParser.BigBed import BigBed
 
-# __author__ = "jkanche, elgaml"
-# __copyright__ = "jkanche"
-# __license__ = "mit"
+__author__ = "jkanche, elgaml"
+__copyright__ = "jkanche"
+__license__ = "mit"
 
-# """
-#     The file test.bigBed and test assertions
-#     come from the pyBigWig library
-# """
+"""
+    The file test.bigBed and test assertions
+    come from the pyBigWig library
+"""
 
-# bb = BigBed("https://obj.umiacs.umd.edu/bigwig-files/ENCFF330GHF.bigBed")
-
-
-# def test_correct_format():
-#     assert (bb.header['magic'] == 2273964779)
+bb = BigBed("https://obj.umiacs.umd.edu/bigwig-files/ENCFF330GHF.bigBed")
 
 
-# def test_header():
-#     assert(bb.header == {'magic': 2273964779, 'version': 4, 'zoomLevels': 10, 'chromTreeOffset': 953, 'fullDataOffset': 1275,
-#            'fullIndexOffset': 142913985, 'fieldCount': 9, 'definedFieldCount': 9, 'autoSqlOffset': 304, 'totalSummaryOffset': 849, 'uncompressBufSize': 29537})
+def test_correct_format():
+    assert (bb.header['magic'] == 2273964779)
 
 
-# def test_columns():
-#     assert(len(bb.columns) == bb.header['fieldCount'])
-#     # assert(bb.columns == ['chr', 'start', 'end', 'name', 'score', 'strand', 'thickStart',7 'thickEnd', 'reserved'])
+def test_header():
+    assert(bb.header == {'magic': 2273964779, 'version': 4, 'zoomLevels': 10, 'chromTreeOffset': 953, 'fullDataOffset': 1275,
+           'fullIndexOffset': 142913985, 'fieldCount': 9, 'definedFieldCount': 9, 'autoSqlOffset': 304, 'totalSummaryOffset': 849, 'uncompressBufSize': 29537})
 
 
-# def test_range():
-#     start = 10000000
-#     end = 10020000
-#     res, err = bb.getRange(chr="chr1", start=start, end=end)
-#     assert(err == None)
-#     for _, row in res.iterrows():
-#         assert (row['start'] <= end or row['end'] >= start)
+def test_columns():
+    assert(len(bb.columns) == bb.header['fieldCount'])
+    # assert(bb.columns == ['chr', 'start', 'end', 'name', 'score', 'strand', 'thickStart',7 'thickEnd', 'reserved'])
 
 
-# def test_get_bytes():
-#     res = bb.get_bytes(1, 100)
-#     assert (len(res) == 100)
+def test_range():
+    start = 10000000
+    end = 10020000
+    res, err = bb.getRange(chr="chr1", start=start, end=end)
+    assert(err == None)
+    for _, row in res.iterrows():
+        assert (row['start'] <= end or row['end'] >= start)
 
 
-# def test_bin_rows():
-#     result, err = bb.getRange(
-#         chr="chr1", start=10000000, end=10010000, bins=2000, zoomlvl=-2)
-#     res, err = bb.bin_rows(data=result, chr="chr1",
-#                            start=10000000, end=10010000, columns=['score'])
-#     assert (err == None)
+def test_get_bytes():
+    res = bb.get_bytes(1, 100)
+    assert (len(res) == 100)
 
 
-# def test_groupBy():
-#     res, err = bb.getRange(chr="chr1", start=10007800,
-#                            end=10010200, bins=2000, zoomlvl=-2)
-#     print(res)
-#     result = bb.groupBy(res, column="name")
-#     print(result)
-#     first_row = True
-#     valid = True
-#     for _, row in result.iterrows():
-#         if first_row:
-#             prev_name = row['name']
-#             prev_end = row['end']
-#             first_row = False
-#         else:
-#             # The name of the next row must be different
-#             if prev_name == row['name']:
-#                 valid = False
-#                 break
-#             # The start of the next row must be greater than or equal to the end of the previour record
-#             if row['start'] < prev_end:
-#                 valid = False
-#                 break
-#             prev_name = row['name']
-#             prev_end = row['end']
+def test_bin_rows():
+    result, err = bb.getRange(
+        chr="chr1", start=10000000, end=10010000, bins=2000, zoomlvl=-2)
+    res, err = bb.bin_rows(data=result, chr="chr1",
+                           start=10000000, end=10010000, columns=['score'])
+    assert (err == None)
 
-#     assert (valid)
+
+def test_groupBy():
+    res, err = bb.getRange(chr="chr1", start=10007800,
+                           end=10010200, bins=2000, zoomlvl=-2)
+    print(res)
+    result = bb.groupBy(res, column="name")
+    print(result)
+    first_row = True
+    valid = True
+    for _, row in result.iterrows():
+        if first_row:
+            prev_name = row['name']
+            prev_end = row['end']
+            first_row = False
+        else:
+            # The name of the next row must be different
+            if prev_name == row['name']:
+                valid = False
+                break
+            # The start of the next row must be greater than or equal to the end of the previour record
+            if row['start'] < prev_end:
+                valid = False
+                break
+            prev_name = row['name']
+            prev_end = row['end']
+
+    assert (valid)
--- tests/test_simplified_bin_rows.py
+++ tests/test_simplified_bin_rows.py
@@ -1,88 +1,112 @@
-# import pytest
-# import os
-# import pandas as pd
-# import json
-# import statistics
-# from epivizFileParser import BigWig
-# bb = BigWig("https://obj.umiacs.umd.edu/bigwig-files/39031.bigwig")
-
-# @pytest.mark.skip(reason="skip")
-# def test_case_1():
-#     data = []
-#     data.append(['chr1',1,10,5])
-#     data.append(['chr1',10,100,9])
-#     data.append(['chr1',100,200,20])
-#     data.append(['chr1',200,210,9])
-#     result = bb.simplify_data(data=data,result_type="mean")
-#     assert len(result)==1
-
-#     assert result[0]['start'] ==1 and result[0]['end']==210 and round(result[0]['score'],3)==10.750
-
-#     result = bb.simplify_data(data=data, result_type="weighted_mean")
-#     assert len(result) == 1
-
-#     assert result[0]['start'] == 1 and result[0]['end']==210 and round(result[0]['score'], 3) == 14.229
-
-# @pytest.mark.skip(reason="skip")
-# def test_case_2():
-#     data = []
-#     data.append(['chr1',1,10,5])
-#     data.append(['chr1',10,100,9])
-#     data.append(['chr1',120,200,20])
-#     data.append(['chr1',200,210,9])
-#     result = bb.simplify_data(data=data,result_type="mean")
-#     assert len(result) == 2
-#     assert result[0]['start'] == 1 and result[0]['end'] == 100 and round(result[0]['score'], 3) == 7.000
-#     assert result[1]['start'] == 120 and result[1]['end'] == 210 and round(result[1]['score'], 3) == 14.500
-#     result = bb.simplify_data(data=data, result_type="weighted_mean")
-#     assert len(result) == 2
-#     assert result[0]['start'] == 1 and result[0]['end'] == 100 and round(result[0]['score'], 3) == 8.690
-#     assert result[1]['start'] == 120 and result[1]['end'] == 210 and round(result[1]['score'], 3) == 18.890
-
-# @pytest.mark.skip(reason="skip")
-# def test_case_3():
-#     data = []
-#     data.append(['chr1',1,10,5])
-#     result = bb.simplify_data(data=data,result_type="mean")
-#     assert len(result) == 1
-#     assert result[0]['start'] == 1 and result[0]['end'] == 10 and round(result[0]['score'], 3) == 5.000
-#     result = bb.simplify_data(data=data, result_type="weighted_mean")
-#     assert len(result) == 1
-#     assert result[0]['start'] == 1 and result[0]['end'] == 10 and round(result[0]['score'], 3) == 5.000
+import pytest
+import os
+import pandas as pd
+import json
+import statistics
+from epivizFileParser import BigWig
+bb = BigWig("https://obj.umiacs.umd.edu/bigwig-files/39031.bigwig")
 
 
-# def test_case_4():
-#     data = []
-#     data.append(['chr1',1,10,5])
-#     data.append(['chr1',11,100,9])
-#     data.append(['chr1',120,200,20])
-#     data.append(['chr1',201,210,9])
-#     result = bb.simplify_data(data=data,result_type="mean")
-#     assert len(result) == 4
-#     assert result[0]['start'] == 1 and result[0]['end'] == 10 and round(result[0]['score'], 3) == 5.000
-#     assert result[1]['start'] == 11 and result[1]['end'] == 100 and round(result[1]['score'], 3) == 9.000
-#     assert result[2]['start'] == 120 and result[2]['end'] == 200 and round(result[2]['score'], 3) == 20.000
-#     assert result[3]['start'] == 201 and result[3]['end'] == 210 and round(result[3]['score'], 3) == 9.000
+@pytest.mark.skip(reason="skip")
+def test_case_1():
+    data = []
+    data.append(['chr1', 1, 10, 5])
+    data.append(['chr1', 10, 100, 9])
+    data.append(['chr1', 100, 200, 20])
+    data.append(['chr1', 200, 210, 9])
+    result = bb.simplify_data(data=data, result_type="mean")
+    assert len(result) == 1
 
-#     result = bb.simplify_data(data=data, result_type="weighted_mean")
-#     assert len(result) == 4
-#     assert result[0]['start'] == 1 and result[0]['end'] == 10 and round(result[0]['score'], 3) == 5.000
-#     assert result[1]['start'] == 11 and result[1]['end'] == 100 and round(result[1]['score'], 3) == 9.000
-#     assert result[2]['start'] == 120 and result[2]['end'] == 200 and round(result[2]['score'], 3) == 20.000
-#     assert result[3]['start'] == 201 and result[3]['end'] == 210 and round(result[3]['score'], 3) == 9.000
+    assert result[0]['start'] == 1 and result[0]['end'] == 210 and round(
+        result[0]['score'], 3) == 10.750
 
-# def test_case_5():
-#     data = []
-#     data.append(['chr1',1,10,5])
-#     data.append(['chr1',10,100,9])
-#     data.append(['chr1',100,200,20])
-#     data.append(['chr1',201,210,9])
-#     result = bb.simplify_data(data=data,result_type="mean")
-#     assert len(result) == 2
-#     assert result[0]['start'] == 1 and result[0]['end'] == 200 and round(result[0]['score'], 3) == 11.333
-#     assert result[1]['start'] == 201 and result[1]['end'] == 210 and round(result[1]['score'], 3) == 9.000
+    result = bb.simplify_data(data=data, result_type="weighted_mean")
+    assert len(result) == 1
 
-#     result = bb.simplify_data(data=data, result_type="weighted_mean")
-#     assert len(result) == 2
-#     assert result[0]['start'] == 1 and result[0]['end'] == 200 and round(result[0]['score'], 3) == 14.445
-#     assert result[1]['start'] == 201 and result[1]['end'] == 210 and round(result[1]['score'], 3) == 9.000
+    assert result[0]['start'] == 1 and result[0]['end'] == 210 and round(
+        result[0]['score'], 3) == 14.229
+
+
+@pytest.mark.skip(reason="skip")
+def test_case_2():
+    data = []
+    data.append(['chr1', 1, 10, 5])
+    data.append(['chr1', 10, 100, 9])
+    data.append(['chr1', 120, 200, 20])
+    data.append(['chr1', 200, 210, 9])
+    result = bb.simplify_data(data=data, result_type="mean")
+    assert len(result) == 2
+    assert result[0]['start'] == 1 and result[0]['end'] == 100 and round(
+        result[0]['score'], 3) == 7.000
+    assert result[1]['start'] == 120 and result[1]['end'] == 210 and round(
+        result[1]['score'], 3) == 14.500
+    result = bb.simplify_data(data=data, result_type="weighted_mean")
+    assert len(result) == 2
+    assert result[0]['start'] == 1 and result[0]['end'] == 100 and round(
+        result[0]['score'], 3) == 8.690
+    assert result[1]['start'] == 120 and result[1]['end'] == 210 and round(
+        result[1]['score'], 3) == 18.890
+
+
+@pytest.mark.skip(reason="skip")
+def test_case_3():
+    data = []
+    data.append(['chr1', 1, 10, 5])
+    result = bb.simplify_data(data=data, result_type="mean")
+    assert len(result) == 1
+    assert result[0]['start'] == 1 and result[0]['end'] == 10 and round(
+        result[0]['score'], 3) == 5.000
+    result = bb.simplify_data(data=data, result_type="weighted_mean")
+    assert len(result) == 1
+    assert result[0]['start'] == 1 and result[0]['end'] == 10 and round(
+        result[0]['score'], 3) == 5.000
+
+
+def test_case_4():
+    data = []
+    data.append(['chr1', 1, 10, 5])
+    data.append(['chr1', 11, 100, 9])
+    data.append(['chr1', 120, 200, 20])
+    data.append(['chr1', 201, 210, 9])
+    result = bb.simplify_data(data=data, result_type="mean")
+    assert len(result) == 4
+    assert result[0]['start'] == 1 and result[0]['end'] == 10 and round(
+        result[0]['score'], 3) == 5.000
+    assert result[1]['start'] == 11 and result[1]['end'] == 100 and round(
+        result[1]['score'], 3) == 9.000
+    assert result[2]['start'] == 120 and result[2]['end'] == 200 and round(
+        result[2]['score'], 3) == 20.000
+    assert result[3]['start'] == 201 and result[3]['end'] == 210 and round(
+        result[3]['score'], 3) == 9.000
+
+    result = bb.simplify_data(data=data, result_type="weighted_mean")
+    assert len(result) == 4
+    assert result[0]['start'] == 1 and result[0]['end'] == 10 and round(
+        result[0]['score'], 3) == 5.000
+    assert result[1]['start'] == 11 and result[1]['end'] == 100 and round(
+        result[1]['score'], 3) == 9.000
+    assert result[2]['start'] == 120 and result[2]['end'] == 200 and round(
+        result[2]['score'], 3) == 20.000
+    assert result[3]['start'] == 201 and result[3]['end'] == 210 and round(
+        result[3]['score'], 3) == 9.000
+
+
+def test_case_5():
+    data = []
+    data.append(['chr1', 1, 10, 5])
+    data.append(['chr1', 10, 100, 9])
+    data.append(['chr1', 100, 200, 20])
+    data.append(['chr1', 201, 210, 9])
+    result = bb.simplify_data(data=data, result_type="mean")
+    assert len(result) == 2
+    assert result[0]['start'] == 1 and result[0]['end'] == 200 and round(
+        result[0]['score'], 3) == 11.333
+    assert result[1]['start'] == 201 and result[1]['end'] == 210 and round(
+        result[1]['score'], 3) == 9.000
+
+    result = bb.simplify_data(data=data, result_type="weighted_mean")
+    assert len(result) == 2
+    assert result[0]['start'] == 1 and result[0]['end'] == 200 and round(
+        result[0]['score'], 3) == 14.445
+    assert result[1]['start'] == 201 and result[1]['end'] == 210 and round(
+        result[1]['score'], 3) == 9.000
